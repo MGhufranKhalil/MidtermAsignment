48b4eaaf64dbdf9870d230484454cb4299139eef
MGhufranKhalil <ghufranrana95@gmail.com>
2019-04-14 15:28:46 +0500

    iniated project

---
+++ src/basic.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+ 
+export default class Basic extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}> </Text>
+      </View>
+    );
+  }
+}
+ 
